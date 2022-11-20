42d5dfcabd9cfc051ee1c96a53ea0abac3d8b1e1
Ubuntu <ubuntu@ip-172-31-23-111.ec2.internal>
2022-11-20 00:27:40 +0000

    Arrumando bug docker

--- script-docker.sh
+++ script-docker.sh
@@ -10,12 +10,16 @@ then
         sudo systemctl start docker
         echo "2 parte"
 sudo systemctl enable docker
-        echo "Buildando DockerFile"
+sudo docker pull mysql
+	echo "Buildando DockerFile"
 sudo docker build -t mysql-safespace .
     sleep 4
         echo "Subindo Container"
         sudo docker run -dit -p 3306:3306 --name safespace mysql-safespace
 else
+sudo docker pull mysql
+  echo "Buildando DockerFile"
+sudo docker build -t mysql-safespace .
     echo "Subindo Container"   
 sudo docker run -dit -p 3306:3306 --name safespacemyql mysql-safespace 
 fi	
